be1aaf5d6254528afebea8daf85e2eb113b80ee1
ElianRmz <elianramirezg24@gmail.com>
2024-07-04 20:39:24 -0600

    js pa la tablona

---
+++ JS/table.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const myTablona = [
+{
+  'medicamento': 'Paracetamol',
+  'caducidad': '07/2024',
+  'cantidad': '10',
+  'dosis': '1 tableta',
+  'proximo': 'Jueves',
+  'fin': '30/05'
+},
+{
+  'medicamento': 'Ibuprofeno',
+  'caducidad': '12/2024',
+  'cantidad': '20',
+  'dosis': '2 tabletas',
+  'proximo': 'Viernes',
+  'fin': '15/06'
+},
+{
+  'medicamento': 'Ibuprofeno',
+  'caducidad': '12/2024',
+  'cantidad': '20',
+  'dosis': '2 tabletas',
+  'proximo': 'Viernes',
+  'fin': '15/06'
+},{
+  'medicamento': 'Ibuprofeno',
+  'caducidad': '12/2024',
+  'cantidad': '20',
+  'dosis': '2 tabletas',
+  'proximo': 'Viernes',
+  'fin': '15/06'
+}
+// Puedes seguir añadiendo más objetos aquí
+];
+
+  function buildTable(data) {
+      const table = document.getElementById('laTablona');
+      for (let i = 0; i < data.length; i++) {
+          const row = `<tr class="bg-gray-200 text-xl">
+              <td class="border border-gray-300 p-4 align-top text">${data[i].medicamento}</td>
+              <td class="border border-gray-300 p-4 align-top">${data[i].caducidad}</td>
+              <td class="border border-gray-300 p-4 align-top">${data[i].cantidad}</td>
+              <td class="border border-gray-300 p-4 align-top">${data[i].dosis}</td>
+              <td class="border border-gray-300 p-4 align-top">${data[i].proximo}</td>
+              <td class="border border-gray-300 p-4 align-top">${data[i].fin}</td>
+          </tr>`;
+          table.innerHTML += row;
+      }
+  }
+
+  buildTable(myTablona);
+});
