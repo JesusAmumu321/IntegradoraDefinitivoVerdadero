a87c9b526cb272ae35f14d87946b0aee95a82585
Jesus Amumu God Jungla <asbedso.320@gmail.com>
2024-07-08 15:10:58 -0600

    Modificacion nombres

--- JS/table.js
+++ JS/table.js
@@ -24,30 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
   'proximo': 'Viernes',
   'fin': '15/06'
 },
-{
-  'medicamento': 'Ibuprofeno2',
-  'caducidad': '12/2024',
-  'cantidad': '20',
-  'dosis': '2 tabletas',
-  'proximo': 'Viernes',
-  'fin': '15/06'
-},
-{
-  'medicamento': 'Ibuprofeno2',
-  'caducidad': '12/2024',
-  'cantidad': '20',
-  'dosis': '2 tabletas',
-  'proximo': 'Viernes',
-  'fin': '15/06'
-},
-{
-  'medicamento': 'Ibuprofeno2',
-  'caducidad': '12/2024',
-  'cantidad': '20',
-  'dosis': '2 tabletas',
-  'proximo': 'Viernes',
-  'fin': '15/06'
-},
 // Puedes seguir añadiendo más objetos aquí
 ];
 
