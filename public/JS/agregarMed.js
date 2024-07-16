3bd4f4585e3ff2e2b9c43e435178b8d0f3b4c056
JesusAmumu321 <asbedso.320@gmail.com>
2024-07-16 15:43:36 -0600

    FALTA ARREGLAR EL JS DE AGREGARMED.JS

--- public/JS/agregarMed.js
+++ public/JS/agregarMed.js
@@ -1,41 +1,30 @@
-// Función para inicializar el formulario de medicamentos
-async function inicializarFormularioMedicamentos() {
+function inicializarFormularioMedicamentos() {
+  
   const tipoMedicamento = document.getElementById("tipo_medicamento");
-  const cantidadDosisDiv = document.getElementById("cantidadDosis").parentNode;
-  const cantidadUnaCajaDiv =
-    document.getElementById("cantidadUnaCaja").parentNode;
+  const cantidadDosisLabel = document.querySelector(
+    'label[for="cantidadDosis"]'
+  );
+  const cantidadDosisInput = document.getElementById("cantidadDosis");
+  const cantidadUnaCajaLabel = document.querySelector(
+    'label[for="cantidadUnaCaja"]'
+  );
+  const cantidadUnaCajaInput = document.getElementById("cantidadUnaCaja");
 
   tipoMedicamento.addEventListener("change", function () {
     if (this.value === "pastillas") {
-      cantidadDosisDiv.innerHTML = `
-              <label class="mb-1 text-m font-medium">
-                Cantidad de pastillas por dosis <span class="text-red-500">*</span>
-              </label>
-              <input type="number" name="cantidadDosis" id="cantidadDosis" required placeholder="Ej. 1."
-                class="w-full px-1.5 h-12 py-2 text-base md:text-l border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black transition duration-300 ease-in-out transform hover:scale-105">
-            `;
-      cantidadUnaCajaDiv.innerHTML = `
-              <label class="mb-1 text-m font-medium">
-                Cantidad de pastillas que contiene 1 caja. <span class="text-red-500">*</span>
-              </label>
-              <input type="number" name="cantidadUnaCaja" id="cantidadUnaCaja" required placeholder="Ej. 30."
-                class="w-full px-1.5 h-12 py-2 text-base md:text-l border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black transition duration-300 ease-in-out transform hover:scale-105">
-            `;
+      cantidadDosisLabel.innerHTML =
+        'Cantidad de pastillas por dosis <span class="text-red-500">*</span>';
+      cantidadDosisInput.placeholder = "Ej. 1.";
+      cantidadUnaCajaLabel.innerHTML =
+        'Cantidad de pastillas que contiene 1 caja. <span class="text-red-500">*</span>';
+      cantidadUnaCajaInput.placeholder = "Ej. 30.";
     } else if (this.value === "ml") {
-      cantidadDosisDiv.innerHTML = `
-              <label class="mb-1 text-m font-medium">
-                Cantidad de mililitros por dosis <span class="text-red-500">*</span>
-              </label>
-              <input type="number" name="cantidadDosis" id="cantidadDosis" required placeholder="Ej. 5."
-                class="w-full px-1.5 h-12 py-2 text-base md:text-l border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black transition duration-300 ease-in-out transform hover:scale-105">
-            `;
-      cantidadUnaCajaDiv.innerHTML = `
-              <label class="mb-1 text-m font-medium">
-                Cantidad de mililitros que contiene 1 frasco. <span class="text-red-500">*</span>
-              </label>
-              <input type="number" name="cantidadUnaCaja" id="cantidadUnaCaja" required placeholder="Ej. 100."
-                class="w-full px-1.5 h-12 py-2 text-base md:text-l border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black transition duration-300 ease-in-out transform hover:scale-105">
-            `;
+      cantidadDosisLabel.innerHTML =
+        'Cantidad de mililitros por dosis <span class="text-red-500">*</span>';
+      cantidadDosisInput.placeholder = "Ej. 5.";
+      cantidadUnaCajaLabel.innerHTML =
+        'Cantidad de mililitros que contiene 1 frasco. <span class="text-red-500">*</span>';
+      cantidadUnaCajaInput.placeholder = "Ej. 100.";
     }
   });
 
@@ -69,8 +58,7 @@ async function inicializarFormularioMedicamentos() {
         if (data.success) {
           Swal.fire({
             icon: "success",
-            title: "Se agrego el medicamento de manera correcta.",
-            // esto es pa q no se pueda quitar la alerta si lepicas afuera de la misam
+            title: "Se agregó el medicamento de manera correcta.",
             allowOutsideClick: false,
           });
           // Aquí puedes limpiar el formulario o redirigir a otra página
@@ -78,7 +66,6 @@ async function inicializarFormularioMedicamentos() {
           Swal.fire({
             icon: "error",
             title: "Error al agregar el medicamento.",
-                        // esto es pa q no se pueda quitar la alerta si lepicas afuera de la misam
             allowOutsideClick: false,
           });
         }
@@ -86,10 +73,8 @@ async function inicializarFormularioMedicamentos() {
         console.error("Error:", error);
         Swal.fire({
           icon: "error",
-          title: "Ocurrio un error, porfavor, reintentelo.",
-          text: `${data.mensaje}`,
-          button: "Ok",
-          // esto es pa q no se pueda quitar la alerta si lepicas afuera de la misam
+          title: "Ocurrió un error, por favor, inténtelo de nuevo.",
+          text: error.message,
           allowOutsideClick: false,
         });
       }
